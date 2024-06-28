31faffa5caf7eb5eaf88a0c10e630a25e44af841
Zach0411 <zachstautz@gmail.com>
2024-06-28 12:08:00 -0400

    hw

---
+++ montecarlo/montecarlo.py
@@ -0,0 +1,19 @@
+# Montecarlo Project
+
+class Die():
+    '''docstring'''
+    
+    def __init__():
+        pass
+
+class Game():
+    '''docstring'''
+    
+    def __init__():
+        pass
+
+class Analyzer():
+    '''docstring'''
+    
+    def __init__():
+        pass
